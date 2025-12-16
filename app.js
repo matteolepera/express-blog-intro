2752ac996995b861bf899a684b25ed8af27d29f2
matteolpera <matteo.lepera02@gmail.com>
2025-12-16 14:53:16 +0100

    feat: aggiunta / con un semplice messaggio.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+import { log } from "console";
 import { ferrariWins } from "./data.js";
 import express from "express";
 
 const app = express();
-const port = 3333;
+const port = 4416;
 
-app.listen((port, () => {
+app.get("/", (req, res) => {
+    res.send("Server bootstrappato by MatteoStrap")
+
+})
+
+app.listen(port, () => {
     console.log(`Il server è pronto a bootstrappare sulla porta ${port} :)`)
-}))
+})
